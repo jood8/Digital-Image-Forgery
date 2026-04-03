b7a35fc6727bdbac080cdd81616062a3414e54b5
Jood mohammad <133890885+jood8@users.noreply.github.com>
2026-04-03 18:39:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,11 @@ st.set_page_config(page_title="Forensic Image Authenticator", layout="wide")
 
 # تحميل الموديل 
 def load_assets():
-    # هذا السطر رح يطبع لنا كل الملفات اللي السيرفر شايفها في المجلد
-    files = os.listdir(".")
-    st.write("📁 الملفات الموجودة في السيرفر حالياً:", files)
-    
-    if "model.pkl" in files:
-        st.success("✅ الملف موجود! جاري التحميل...")
-        try:
-            data = joblib.load("model.pkl")
-            return data
-        except Exception as e:
-            st.error(f"❌ خطأ أثناء تحميل الموديل: {e}")
-            st.stop()
-    else:
-        st.error("🚨 الملف 'model.pkl' غير موجود في قائمة الملفات أعلاه!")
+    try:
+        data = joblib.load("model.pkl")
+        return data
+    except:
+        st.error("🚨 ملف model.pkl غير موجود! تأكدي من تشغيل كود التدريب أولاً.")
         st.stop()
 
 assets = load_assets()
